ec3e68d1b78de862db71c283d826d6411b67f420
Senechal Thibaut <93082381+Nerion-1337@users.noreply.github.com>
2023-08-16 18:38:10 +0200

    Update SQL.js

--- back/SQL.js
+++ back/SQL.js
@@ -4,6 +4,6 @@ const mysql = require("mysql2");
 module.exports.SQL = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"Gotham21087*",
-    database:"test"
+    password:"A MODIFIER",
+    database:"A MODIFIER"
 })
